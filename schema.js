881df76b17bd89b06717c011ab284b283f839506
Arthur Khutov <arthur-khutov@MacBook-Pro-3.local>
2024-12-12 12:24:43 +0300

    first commit

---
+++ schema.js
@@ -0,0 +1,42 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+        reviews: [Review!]
+    }
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+        game: Game!
+        author: Author!
+    }
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+        reviews: [Review!]
+    }
+    type Query {
+        games: [Game]
+        game(id: ID!): Game
+        reviews: [Review]
+        review(id: ID!): Review
+        authors: [Author]
+        author(id: ID!): Author
+    }
+    type Mutation {
+        deleteGame(id: ID!): [Game]
+        addGame(game: addGame): Game
+        updateGame(id: ID!, edits: updateGame): Game
+    }
+    input addGame {
+        title: String!
+        platform: [String!]!
+    }
+    input updateGame {
+        title: String
+        platform: [String]
+    }
+`
